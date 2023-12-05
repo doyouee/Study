50089b7360dec378df032b61b6a64a5c6b9a3177
doyouee <aquariu92s@naver.com>
2023-12-05 13:38:14 +0900

    Socket을 활용한 간단한 프로그램 구현(Client 및 Listener)

---
+++ websocket/src/main/java/com/websocket/basic/BasicListener/Listener.java
@@ -0,0 +1,24 @@
+package com.websocket.basic.BasicListener;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Listener {
+	public static void main(String[] args) throws IOException {
+		// client -> listener
+		ServerSocket serversocket = new ServerSocket(5555); // 5555port 번호로 서버 소켓 만들기
+		System.out.println("접 속 대 기");
+		Socket socket = serversocket.accept(); // serversocket이 accept 할 때 발생
+		DataInputStream sdin = new DataInputStream(socket.getInputStream());
+		System.out.println(sdin.readUTF()); // Client에서 output한 Data가 찍혀나온다.
+		
+		// listener -> client
+		DataOutputStream sdout = new DataOutputStream(socket.getOutputStream());
+		sdout.writeUTF("Hello Client");
+		
+		serversocket.close();
+	}
+}
